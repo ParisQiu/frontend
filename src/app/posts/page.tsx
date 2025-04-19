0089120c343c594cdaf9d8ee94c4375cd193e61c
ParisQiu <paris.qiu522@gmail.com>
2025-04-18 17:35:06 -0700

    Update page.tsx

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,11 @@
+// src/app/posts/page.tsx
+import React from 'react';
+
+export default function PostsPage() {
+  return (
+    <main>
+      <h1>All Posts</h1>
+      {/* …your markup… */}
+    </main>
+  );
+}
